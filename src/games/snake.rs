006c399dd7d9e67afb3b590305decb148f3515a5
Xertes0 <55015543+Xertes0@users.noreply.github.com>
2022-04-25 20:00:35 +0200

    heapless::Vec

--- src/games/snake.rs
+++ src/games/snake.rs
@@ -6,7 +6,7 @@ use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::{Point, Size, Primitive};
 use embedded_graphics::primitives::{Rectangle, PrimitiveStyle};
 use embedded_graphics::text::Text;
-use heapless::String;
+use heapless::{String, Vec};
 
 use crate::inputs::Inputs;
 use crate::pcd8544::PCD8544;
@@ -15,7 +15,7 @@ use crate::sfx::inverse_blink::inverse_blink;
 
 const MAX_SIZE: usize = 100;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 enum Direction {
     Left,
     Down,
@@ -27,24 +27,27 @@ const SCALE:  usize = 2;
 const GRID_X: usize = 84/SCALE;
 const GRID_Y: usize = 48/SCALE;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Tile {
     pub pos: Point,
     pub dir: Direction,
 }
 
 struct Snake {
-    tail: [Tile; MAX_SIZE],
-    tail_count: usize,
-    stale: usize,
+    tail: Vec<Tile, MAX_SIZE>,
+    stale: Option<Tile>,
 }
 
 impl Snake {
     pub fn new() -> Self {
         Self {
-            tail: [Tile{ pos: Point::new(GRID_X as i32/2 - 1, GRID_Y as i32/2), dir: Direction::Right }; MAX_SIZE],
-            tail_count: 1,
-            stale: 0,
+            tail: Vec::from_slice(
+              &[ Tile{
+                    pos: Point::new(GRID_X as i32/2 - 1, GRID_Y as i32/2),
+                    dir: Direction::Right,
+                  }
+              ]).unwrap(),
+            stale: None,
         }
     }
 
@@ -53,64 +56,48 @@ impl Snake {
     }
 
     pub fn draw(&self, pcd: &mut PCD8544) {
-        for i in 0..self.tail_count {
-            Rectangle::new(self.tail[i].pos * SCALE as i32, Size::new(SCALE as u32, SCALE as u32))
+        for tile in &self.tail {
+            Rectangle::new(tile.pos * SCALE as i32, Size::new(SCALE as u32, SCALE as u32))
                 .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
                 .draw(pcd).unwrap();
         }
     }
 
-    fn check_collision(&self, index: usize) -> bool {
-        for i in 0..self.tail_count-self.stale {
-            if i == index {continue;}
-            if self.tail[i].pos.x == self.tail[index].pos.x &&
-               self.tail[i].pos.y == self.tail[index].pos.y {
-                   return true;
-            }
-        }
-
-        false
-    }
-
     pub fn update(&mut self) -> bool {
         let mut next_dir = self.tail[0].dir;
-        for i in 0..self.tail_count-self.stale {
-
+        for i in 0..self.tail.len() {
             match self.tail[i].dir {
                 Direction::Left => {
                     if self.tail[i].pos.x == 1 {
                         return false;
                     }
                     self.tail[i].pos.x -= 1;
-                    if self.check_collision(i) {
-                        return false;
-                    }
                 },
                 Direction::Up => {
                     if self.tail[i].pos.y == 1 {
                         return false;
                     }
                     self.tail[i].pos.y -= 1;
-                    if self.check_collision(i) {
-                        return false;
-                    }
                 },
                 Direction::Down => {
                     if self.tail[i].pos.y == GRID_Y as i32 - 2 {
                         return false;
                     }
                     self.tail[i].pos.y += 1;
-                    if self.check_collision(i) {
-                        return false;
-                    }
                 },
                 Direction::Right => {
                     if self.tail[i].pos.x == GRID_X as i32 - 2 {
                         return false;
                     }
                     self.tail[i].pos.x += 1;
-                    if self.check_collision(i) {
-                        return false;
+                }
+            }
+
+            if i == 0 {
+                for other in self.tail.iter().skip(1) {
+                    if other.pos.x == self.tail[i].pos.x &&
+                       other.pos.y == self.tail[i].pos.y {
+                           return false;
                     }
                 }
             }
@@ -119,9 +106,10 @@ impl Snake {
             next_dir = tmp;
         }
 
-        if self.stale > 0 { // assume stale can't be > 1
-            self.stale -= 1;
-            self.tail[self.tail_count].dir = next_dir;
+        if let Some(mut stale) = self.stale { // assume stale can't be > 1
+            stale.dir = next_dir;
+            self.tail.push(stale).unwrap();
+            self.stale = None;
         }
 
         true
@@ -129,9 +117,7 @@ impl Snake {
 
     pub fn eat_apple(&mut self, apple: Point) -> bool {
         if self.tail[0].pos == apple {
-            self.tail[self.tail_count] = self.tail[self.tail_count-1];
-            self.tail_count += 1;
-            self.stale += 1;
+            self.stale = Some(self.tail[self.tail.len() - 1]);
             return true;
         }
 
@@ -269,7 +255,7 @@ impl<'a> SnakeGame<'a> {
         self.apple.draw(self.pcd);
 
         // Score
-        let x_size = if self.snake.tail_count > 9 {
+        let x_size = if self.snake.tail.len() > 9 {
             10
         } else {
             5
@@ -278,7 +264,7 @@ impl<'a> SnakeGame<'a> {
         Rectangle::new(Point::new(0,45-7), Size::new(x_size+3, 7+3))
             .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
             .draw(self.pcd).unwrap();
-        Text::new(&String::<3>::from(self.snake.tail_count as u32), Point::new(2,45), MonoTextStyle::new(&FONT_5X7, BinaryColor::On))
+        Text::new(&String::<3>::from(self.snake.tail.len() as u32), Point::new(2,45), MonoTextStyle::new(&FONT_5X7, BinaryColor::On))
             .draw(self.pcd).unwrap();
 
         self.pcd.draw();
